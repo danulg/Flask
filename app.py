c30e47f33a56e1eeb72ae0c18c14a6cde8d959c4
danulg <danulg@gmail.com>
2020-06-24 13:05:52 -0400

    Simple Data Capture

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, request, redirect
-#Attempt at commiting changes
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+
   return render_template('index.html')
 
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+@app.route('/pricechart', methods=['POST', 'GET'])
+def pricechart():
+  name = request.args.get('sname', 'GOOG')
+  msg = "Your input was " + name
+  return render_template('pricechart.html', forward_message=msg)
+
+
 
 if __name__ == '__main__':
   app.run(port=33507)
