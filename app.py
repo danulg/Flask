b08f1680dbd3ef0146ae37025458077c7d103eb7
danulg <danulg@gmail.com>
2020-06-24 16:56:57 -0400

    Bokeh added

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure, show, output_file
+from bokeh.embed import components
+
 
 app = Flask(__name__)
 
@@ -13,9 +16,13 @@ def about():
 
 @app.route('/pricechart', methods=['POST', 'GET'])
 def pricechart():
-  name = request.args.get('sname', 'GOOG')
+  name = request.args.get('sname')
   msg = "Your input was " + name
-  return render_template('pricechart.html', forward_message=msg)
+  fig = figure(title="Sensor data")
+  fig.line([1, 2, 3, 4], [2, 4, 6, 8])
+  script, div = components(fig)
+  #show(fig) -picture is drawn as required. Issues with render template
+  return render_template('pricechart.html', forward_message=msg, div=div, script=script)
 
 
 
