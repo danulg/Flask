a0890ebf448c9435549fcd7019c1fa7367073fc5
danulg <danulg@gmail.com>
2020-06-25 13:19:11 -0400

    Basic data extracted from nested data strcuture. Fixed issue with immediate conversion to dataframe being unsuitable for manipulation. Better fix may be available

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from bokeh.plotting import figure, show, output_file
 from bokeh.embed import components
 import requests
 from pandas.io.json import json_normalize
-import simplejson as json
+import numpy as np
 import pandas as pd
 
 
@@ -11,7 +11,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
   return render_template('index.html')
 
 @app.route('/about')
@@ -20,26 +19,61 @@ def about():
 
 @app.route('/pricechart', methods=['POST', 'GET'])
 def pricechart():
+  #Retrive information from /
   name = request.args.get('sname')
   msg = "Monthly Data for " + name
+
+  #Request data
   monthly_data = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol='+name+'&apikey=SJFMXK64TWAWK71Y'
   dsm = requests.get(monthly_data)
+
+  #format data
   dsm = dsm.json()
-  dsm = pd.DataFrame(dsm)
-  dsm_date = dsm['Meta Data']
-  dsm_ts = dsm['Monthly Time Series'][4:16]
-  #dsm_ts = pd.DataFrame.from_dict(dsm_ts)
-  print('time series is', dsm_ts)
-  print('meta data is', dsm_date)
+  dsm = dsm['Monthly Time Series']
+  dsm = pd.DataFrame.from_dict(json_normalize(dsm))
 
-  
+  #prep slice of data for drawing
+  dsm = dsm.iloc[:,np.r_[0:72]]
+  print(dsm)
+  xs = [0,1,2,3,4,5,6,7,8,9,10,11]
+  ys = [0,0,0,0,0,0,0,0,0,0,0,0]
+  counter = 0
+  col_counter = 1
+  length = len(dsm.columns)
+
+  #Pick slice for drawing: issue with skip length
+  while(col_counter < length) and counter <=11:
+    ys[counter] = dsm.iloc[0, col_counter]
+    print(ys[counter])
+    counter+=1
+    col_counter+=6
 
 
-  fig = figure(title=name)
-  fig.line([1, 2, 3, 4], [2, 4, 6, 8])
+  print(ys)
+
+  #
+
+
+  #dsm = json.load(dsm)
+  #print(type(dsm))
+
+  #dsm = pd.DataFrame(dsm)
+  #dsm_ts = dsm['Monthly Time Series'][4:16]
+  #print('time series is', dsm_ts)
+
+
+
+
+
+  #Draw
+  fig = figure(title=msg)
+  fig.line(xs, ys)
   script, div = components(fig)
   return render_template('pricechart.html', script=script, div=div)
 
+@app.route('/error')
+def error():
+  return render_template('error.html')
 
 
 if __name__ == '__main__':
